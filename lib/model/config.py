1c630703a245e90d1a0e108d73dce9b710c911b1
phil-bergmann <philipp.bergmann@tum.de>
2018-02-05 15:48:04 +0100

    changed output functions now that results are saved to {out}/frcnn

--- lib/model/config.py
+++ lib/model/config.py
@@ -292,7 +292,7 @@ def get_output_dir(imdb, weights_filename):
   A canonical path is built using the name from an imdb and a network
   (if not None).
   """
-  outdir = osp.abspath(osp.join(__C.ROOT_DIR, 'output', __C.EXP_DIR, imdb.name))
+  outdir = osp.abspath(osp.join(__C.ROOT_DIR, 'output', 'frcnn', __C.EXP_DIR, imdb.name))
   if weights_filename is None:
     weights_filename = 'default'
   outdir = osp.join(outdir, weights_filename)
@@ -308,7 +308,7 @@ def get_output_tb_dir(imdb, weights_filename):
   A canonical path is built using the name from an imdb and a network
   (if not None).
   """
-  outdir = osp.abspath(osp.join(__C.ROOT_DIR, 'tensorboard', __C.EXP_DIR, imdb.name))
+  outdir = osp.abspath(osp.join(__C.ROOT_DIR, 'tensorboard', 'frcnn', __C.EXP_DIR, imdb.name))
   if weights_filename is None:
     weights_filename = 'default'
   outdir = osp.join(outdir, weights_filename)
